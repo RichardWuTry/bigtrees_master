1f46dad303d421431612089dcc4f652a39754fc4
DBFocus <shadow_wu82@163.com>
2013-11-21 16:57:26 +0800

    add Article/showDetail page

--- Index/Lib/Action/ArticleAction.class.php
+++ Index/Lib/Action/ArticleAction.class.php
@@ -1,9 +1,16 @@
 <?php
 class ArticleAction extends Action {
-    public function showDetail(){
+    public function showDetail() {
     	if (isset($_GET['id'])) {
     		$article_id = $_GET['id'];
-    		echo $article_id;
+    		$Article = M('Article');
+
+    		if ($a = $Article->where('article_id='.$article_id)
+    								->find()) {
+    			$a['tag_list'] = explode(',', $a['tags']);
+    			$this->assign('article',$a);
+    		}
+    		$this->display();
     	}
     }
 }
