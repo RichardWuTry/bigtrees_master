2747e8b96f89952f12adde8a082dd838b42eaa21
DBFocus <shadow_wu82@163.com>
2013-11-25 16:36:48 +0800

    add newArticle form

--- Index/Lib/Action/AdminAction.class.php
+++ Index/Lib/Action/AdminAction.class.php
@@ -8,7 +8,27 @@ class AdminAction extends Action {
     }
 
     public function listArticle() {
-        echo 'listArticle';
+/*        if (isset($_GET['start_row_no'])) {
+            $start_row_no = $_GET['start_row_no'];
+        } else {
+            $start_row_no = 0;
+        }
+        $Article = M('Article');
+        if ($articles = $Article->order('priority desc, posted_at desc')
+                                ->limit($start_row_no,8)
+                                ->select()) {
+            foreach ($articles as &$a) {
+                $a['tag_list'] = explode(',', $a['tags']);
+            }
+            $this->assign('articles', $articles);
+        }
+
+        $this->display();*/
+    }
+
+    public function newArticle() {
+        $this->assign('username', $_SESSION[APP_PREFIX.'user_name']);
+        $this->display();
     }
 
     public function addArticle() {
