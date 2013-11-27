6cf464f0df0441a18b471cd91d5b2a2a7d268329
DBFocus <shadow_wu82@163.com>
2013-11-27 17:59:42 +0800

    add listInterview function

--- Index/Lib/Action/ArticleAction.class.php
+++ Index/Lib/Action/ArticleAction.class.php
@@ -13,5 +13,34 @@ class ArticleAction extends Action {
     		$this->display();
     	}
     }
+
+    public function listInterview() {
+        if (isset($_GET['tag'])) {
+            $tag = $_GET['tag'];
+            $Model = M();
+            $row_cnt = C('IndexAction.per_page_count');
+            if($articles = $Model->query("
+                select
+                    *
+                from
+                    article
+                where
+                    article_id in (select distinct article_id from tag where tag_name = '{$tag}')
+                    and
+                    is_active = 1
+                order by
+                    priority desc, posted_at desc
+                limit
+                    0,{$row_cnt};
+                ")){
+                foreach ($articles as &$a) {
+                    $a['tag_list'] = explode(',', $a['tags']);
+                }
+                $this->assign('articles', $articles);
+            }
+            $this->assign('tag', $tag);
+            $this->display();
+        }
+    }
 }
 ?>
--- Index/Lib/Action/IndexAction.class.php
+++ Index/Lib/Action/IndexAction.class.php
@@ -8,26 +8,58 @@ class IndexAction extends Action {
 
     public function nextPage(){
         if ($this->isPost()) {
+            $condition = '';
+            if (isset($_GET['tag'])) {
+                $tag = $_GET['tag'];
+                $condition = $condition."
+                            and
+                            article_id in (select article_id from tag where tag_name = '{$tag}')
+                            ";
+            }
             $start_row_no = $_POST['start_row_no'];
-            $this->displayArticles($start_row_no);   
+            $this->displayArticles($start_row_no, $condition);   
         }
     }
 
-    private function displayArticles($start_row_no){
-        $Article = M('Article');
-        if ($articles = $Article->where('is_active=1')
-                                ->order('priority desc, posted_at desc')
-                                ->limit($start_row_no,C('IndexAction.per_page_count'))
-                                ->select()) {
-            foreach ($articles as &$a) {
-                $a['tag_list'] = explode(',', $a['tags']);
-            }
-            $this->assign('articles', $articles);
+    public function displayArticles($start_row_no, $condition=''){
+        $Model = M();
+        $row_cnt = C('IndexAction.per_page_count');
+        if ($articles = $Model->query("
+                select
+                    *
+                from
+                    article
+                where
+                    is_active = 1
+                    {$condition}
+                order by
+                    priority desc, posted_at desc
+                limit
+                    {$start_row_no},{$row_cnt}
+            ")) {
+                foreach ($articles as &$a) {
+                    $a['tag_list'] = explode(',', $a['tags']);
+                }
+                $this->assign('articles', $articles);
         }
         $this->display();
     }
 
-    //关于我们
+    //访谈分类 Index/interview
+    public function interview(){
+        $Model = M();
+        if ($tags = $Model->query("
+                select distinct
+                    tag_name
+                from
+                    tag
+            ")) {
+            $this->assign('tags', $tags);
+        }
+        $this->display();
+    }
+
+    //关于我们 Index/about
     public function about(){
         $this->display();
     }
